f3f15eee9b257bdcf381584daccdee313e026042
RBilanJapp <rbilan@jappware.com>
2022-11-08 21:00:57 +0200

    added login and register pages; cerated Login/RegisterForm; setup register page

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '710d19f48502d56f61334e369fc54202'
 
 posts = [
     {'author': 'Jon Doe',
@@ -17,13 +19,28 @@ posts = [
 
 
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
